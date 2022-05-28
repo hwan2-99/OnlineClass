e3238d023b6622b8d7b20b82817785294e1a2d2a
yeongi <cbg5255@gmail.com>
2022-05-28 14:58:38 +0900

    basic project setting

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>안녕하세요 1년만의 프로젝트</h1>
+        <h1>오늘 부터 열심히 갓생 살아 보즈아!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
