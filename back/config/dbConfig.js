25f735d70f21184864ffdf2940cbd47298ff06bd
yeongi <cbg5255@gmail.com>
2022-06-01 17:53:33 +0900

    dbconnect

---
+++ back/config/dbConfig.js
@@ -0,0 +1,23 @@
+const { createPool } = require("mysql2/promise");
+
+//환경 설정
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const dbInfo = {
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+// };
+
+const dbInfo = {
+  host: "localhost",
+  user: "root",
+  password: "1111",
+  database: "onlineclass",
+};
+
+const pool = createPool(dbInfo);
+
+module.exports = pool;
